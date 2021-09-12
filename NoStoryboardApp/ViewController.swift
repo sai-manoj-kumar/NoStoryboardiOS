d2a6bede3c2675b873be220f492fd92e93c70a93
Sai Manoj Kumar Yadlapati <saiyad@microsoft.com>
2021-09-12 19:54:07 +0530

    Add MainView

--- NoStoryboardApp/ViewController.swift
+++ NoStoryboardApp/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let mainView = MainView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(mainView)
+
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        mainView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
 
 
